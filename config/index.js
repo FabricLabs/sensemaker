b4485319b9642c8f8f16d7a8fac63b8bbafea02d
Eric Martindale <eric@ericmartindale.com>
2015-01-16 12:19:44 -0500

    add example config

---
+++ config/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  twitter: {
+    consumerKey:       'acquire',
+    consumerSecret:    'these',
+    accessTokenKey:    'from',
+    accessTokenSecret: 'dev.twitter.com',
+  },
+  facebook: {
+    appID:             'these',
+    appSecret:         'from',
+    baseURI:           'developers.facebook.com',
+  },
+  google: {
+    key: 'console.developers.google.com'
+  },
+  soundcloud: {
+    clientID: 'dev.soundcloud.com'
+  }
+}
