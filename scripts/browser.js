a170c8c2ecd35dfd2edae131a112d365ad163cad
Eric Martindale <eric@ericmartindale.com>
2022-09-08 22:20:48 -0400

    Add browser script, update package-lock.json

---
+++ scripts/browser.js
@@ -0,0 +1,35 @@
+// Dependencies
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Components
+import Sensemaker from '../components/Sensemaker';
+
+// Settings
+const settings = {
+  currency: 'USD',
+  symbols: [
+    'BTC',
+    'LTC',
+    'NMC'
+  ]
+};
+
+// Main Process Definition
+async function main (input = {}) {
+  const container = document.getElementById('fabric-application-root');
+  const root = createRoot(container);
+
+  root.render(<Sensemaker state={input} />);
+
+  return {
+    react: { root }
+  }
+}
+
+// Run Main Process
+main(settings).catch((exception) => {
+  console.error('[SENSEMAKER:BROWSER] Main Process Exception:', exception);
+}).then((output) => {
+  console.log('[SENSEMAKER:BROWSER] Main Process Output:', output);
+});
