4fa52ade401039915d3728a1cc1b58c1a094c653
Eric Martindale <eric@ericmartindale.com>
2023-05-22 15:18:51 -0400

    Use simpler benchmark

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,21 +1,9 @@
 import time
 import os
-import json
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 
 # Set up GPU for training
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-# Load pre-trained model (weights)
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-model = model.to(device)
-
-# Folder with JSONL files
-folder = "./stores/corpus"
 
 # Time for benchmarking
 start_time = time.time()
@@ -23,28 +11,29 @@ start_time = time.time()
 # Number of sequences processed
 num_sequences = 0
 
-# Process each file in the folder
-for filename in os.listdir(folder):
-    if filename.endswith(".jsonl"):
-        with open(os.path.join(folder, filename), "r") as file:
-            for line in file:
-                data = json.loads(line)
-                prompt = data.get("prompt", "")
-                response = data.get("response", "")
-                
-                # Combine prompt and response into one sequence
-                sequence = prompt + tokenizer.eos_token + response
+# Target directory
+directory = "./stores/corpus"
 
-                # Tokenize sequence and prepare input tensors
-                inputs = tokenizer(sequence, return_tensors="pt")
-                inputs = {k: v.to(device) for k, v in inputs.items()}
+# Process each file in the directory
+for filename in os.listdir(directory):
+    file_path = os.path.join(directory, filename)
+    if os.path.isfile(file_path):
+        with open(file_path, "rb") as file:
+            content = file.read()
 
-                # Perform a forward pass (evaluate the model on this input)
-                outputs = model(**inputs)
+            # Assuming content is in hexadecimal bytes representation,
+            # decode it to bytes
+            try:
+                byte_content = bytes.fromhex(content.decode())
+            except ValueError:
+                print(f"Skipping file: {file_path}. Not in hexadecimal format.")
+                continue
 
-                # Don't perform a backward pass (we're not actually training)
-                # Increment sequence count
-                num_sequences += 1
+            # Process the content as needed
+            # Here, we can assume each byte as a sequence
+
+            # Example: Count the number of sequences
+            num_sequences += len(byte_content)
 
 # Calculate sequences per second
 end_time = time.time()
