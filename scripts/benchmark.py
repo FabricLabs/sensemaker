3d512b6bd050176eae3d6e14fd43594c5ec2ba91
Eric Martindale <eric@ericmartindale.com>
2023-05-22 23:05:34 -0400

    Re-work benchmark

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,63 +1,70 @@
-import time
-import os
 import torch
-import json
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from multiprocessing import Pool
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW
+from torch.utils.data import Dataset, DataLoader
 
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# Check if PyTorch sees your GPUs
+print(torch.cuda.device_count())
 
-# Set the pad_token explicitly
-tokenizer.pad_token = tokenizer.eos_token
+class GPT2Dataset(Dataset):
+    # Simple Dataset class for GPT-2
+    def __init__(self, txt_list, tokenizer, max_length):
+        self.tokenizer = tokenizer
+        self.input_ids = []
+        self.attn_masks = []
+        for txt in txt_list:
+            encodings_dict = tokenizer(txt, truncation=True, max_length=max_length, padding="max_length")
+            self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
+            self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
+    
+    def __len__(self):
+        return len(self.input_ids)
 
-# Load pre-trained model (weights)
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.attn_masks[idx]
 
-# Number of processes (equal to the number of available GPUs)
-num_processes = torch.cuda.device_count()
+def train_on_gpu(device_id):
+    # Select GPU
+    device = torch.device(f"cuda:{device_id}" if torch.cuda.is_available() else "cpu")
+    print(f"Training on: {device}")
+    
+    # Load tokenizer and model
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    model.to(device)
+    
+    # Initialize dataset
+    text_data = ["Some text to train on", "Some other text"]
+    max_length = 128
+    dataset = GPT2Dataset(text_data, tokenizer, max_length)
+    loader = DataLoader(dataset, batch_size=1, shuffle=True)
+    
+    optimizer = AdamW(model.parameters(), lr=1e-4)
+    
+    model.train()
+    for epoch in range(2):
+        print(f"Epoch: {epoch}")
+        for idx, batch in enumerate(loader):
+            input_ids, attention_mask = [b.to(device) for b in batch]
+            outputs = model(input_ids, attention_mask=attention_mask, labels=input_ids)
+            loss = outputs.loss
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
-# Target directory
-directory = "./stores/corpus"
+            if idx % 10 == 0:
+                print(f"Loss: {loss.item()}")
 
-
-def process_file(file_path):
-    with open(file_path, "rb") as file:
-        content = file.read()
-
-        # Convert raw content to hexadecimal bytes
-        hex_content = content.hex()
-
-        # Create metadata dictionary with file name and other details
-        metadata = {"file_name": os.path.basename(file_path)}
-
-        # Embed metadata in the content string
-        content_with_metadata = json.dumps(metadata) + hex_content
-
-        # Tokenize the content with metadata
-        inputs = tokenizer(content_with_metadata, return_tensors="pt", truncation=True, padding=True)
-
-        # Perform a forward pass (evaluate the model on this input)
-        outputs = model(**inputs)
-
-
-if __name__ == "__main__":
-    # Time for benchmarking
-    start_time = time.time()
-
-    # Create a pool of processes
-    pool = Pool(processes=num_processes)
-
-    # Process each file in the directory in parallel
-    file_paths = [os.path.join(directory, filename) for filename in os.listdir(directory) if os.path.isfile(os.path.join(directory, filename))]
-    pool.map(process_file, file_paths)
-
-    # Close the pool
-    pool.close()
-    pool.join()
-
-    # Calculate sequences per second
-    end_time = time.time()
-    sequences_per_second = len(file_paths) / (end_time - start_time)
-
-    print(f"Sequences per second: {sequences_per_second}")
+def main():
+    mode = input("Select mode ([1] single, [2] parallel): ")
+    
+    if mode == '1':
+        gpu_id = input("Select GPU ID: ")
+        train_on_gpu(gpu_id)
+    elif mode == '2':
+        for gpu_id in range(torch.cuda.device_count()):
+            train_on_gpu(gpu_id)
+    else:
+        print("Invalid mode. Please enter either '1' for single or '2' for parallel.")
+        
+if __name__ == '__main__':
+    main()
