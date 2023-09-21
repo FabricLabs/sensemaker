bf23ba20e8f741a0a6691933f21f7ad6b637a944
Eric Martindale <eric@ericmartindale.com>
2023-09-21 08:04:45 -0400

    Fix bug in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,44 +10,26 @@ fi
 echo "Installing the Fabric Node"
 echo "=========================="
 
-echo "> updating system..."
-apt -o Acquire::https::AllowRedirect=false update
-
-echo "> installing dependencies..."
-apt -o Acquire::https::AllowRedirect=false install -y
-  build-essential
-  curl
-  deb.torproject.org-keyring
-  git
-  iptables
-  iptables-persistent
-  jq
-  mysql-server
-  nginx
-  pytorch
-  tor
-  unzip
-  vim
-
-# NVM
-cat scripts/nvm/install.sh | bash
-nvm install 16.17.1
-npm i -g pm2 vtop
-
 echo "Provide the source repository for your project:"
 read GIT_URL
 
 echo "Specify the target directory name:"
 read TARGET_DIRECTORY
 
+# Begin Installation
 echo "> cloning repository..."
 git clone --recurse-submodules "$GIT_URL" "$TARGET_DIRECTORY"
 
 echo "> installing project dependencies..."
 cd "$TARGET_DIRECTORY"
 
+# NVM
+cat scripts/nvm/install.sh | bash
+nvm install 16.17.1
+npm i -g pm2 vtop
+
+# Node
 npm install
-cd ..
 
 # Report to User
 echo "All done!  Now run the following to start your node:"
