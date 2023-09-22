a2035e58dbe55df8d26e08c9716d1c3965ede1e6
nahuelcodingit <nahuel@codingit.dev>
2023-09-22 17:47:07 -0300

    added the token and msgID

--- components/QueryForm.js
+++ components/QueryForm.js
@@ -5,6 +5,8 @@ const React = require('react');
 const $ = require('jquery');
 const marked = require('marked');
 
+const store = require('../stores/redux');
+
 // Semantic UI
 const {
   Button,
@@ -164,13 +166,19 @@ class Chat extends React.Component {
 
   handleModalSend = () => {
     const { rating, comment, thumbsUpClicked, thumbsDownClicked } = this.state;
-    
+    const { message } = this.props.chat;
+    const mssageId = message.id; 
+    const state = store.getState();
+    const token = state.auth.token;
+
     //data to send to the API
     const dataToSend = {
       rating,
       comment,
       thumbsUpClicked,
       thumbsDownClicked,
+      mssageId,
+      token
     };
     
     //shows loading button
@@ -183,7 +191,7 @@ class Chat extends React.Component {
       }, 1500);
     });
 
-    Promise.all([delayPromise, fetch('API-Endpoint', {
+    Promise.all([delayPromise, fetch('/reviews', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
