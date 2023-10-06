829ce4fec3dcd1f024fd9506524eff52d33a4ee0
nahuelcodingit <nahuel@codingit.dev>
2023-10-06 15:50:24 -0300

    fixed dashboard.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 // Dependencies
-const React = require("react");
-const { Link, Navigate, Route, Routes, Switch } = require("react-router-dom");
+const React = require('react');
+const { Link, Navigate, Route, Routes, Switch } = require('react-router-dom');
 // const LoadingBar = require('react-top-loading-bar');
 
 // Semantic UI
@@ -19,67 +19,64 @@ const {
   Search,
   Segment,
   Sidebar,
-} = require("semantic-ui-react");
+} = require('semantic-ui-react');
 
 // const Joyride = require('react-joyride');
 
 // Components
-const Home = require("./Home");
-const CaseHome = require("./CaseHome");
-const CaseView = require("./CaseView");
-const Workspaces = require("./Workspaces");
-const Conversations = require("./Conversations");
-const Room = require("./Room");
-const Settings = require("./Settings");
-const AdminSettings = require("./AdminSettings");
-const TermsOfUse = require("./TermsOfUse");
+const Home = require('./Home');
+const CaseHome = require('./CaseHome');
+const CaseView = require('./CaseView');
+const Workspaces = require('./Workspaces');
+const Conversations = require('./Conversations');
+const Room = require('./Room');
+const Settings = require('./Settings');
+const AdminSettings = require('./AdminSettings');
+const TermsOfUse = require('./TermsOfUse');
 
 // Fabric Bridge
-const Bridge = require("./Bridge");
+const Bridge = require('./Bridge');
 
 class Dashboard extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
 
-    this.settings = Object.assign(
-      {
-        debug: false,
-        state: {
-          loading: false,
-          username: "(guest account)",
-          search: "",
-          sidebarCollapsed: false,
-          sidebarVisible: true,
-          progress: 0,
-          isLoading: true,
-          isLoggingOut: false,
-          steps: [
-            {
-              target: ".my-first-step",
-              content: "This is my awesome feature!",
-            },
-            {
-              target: ".my-other-step",
-              content: "This another awesome feature!",
-            },
-          ],
-        },
-      },
-      props
-    );
+    this.settings = Object.assign({
+      debug: false,
+      state: {
+        loading: false,
+        username: '(guest account)',
+        search: '',
+        sidebarCollapsed: false,
+        sidebarVisible: true,
+        progress: 0,
+        isLoading: true,
+        isLoggingOut: false,
+        steps: [
+          {
+            target: '.my-first-step',
+            content: 'This is my awesome feature!',
+          },
+          {
+            target: '.my-other-step',
+            content: 'This another awesome feature!',
+          }
+        ]
+      }
+    }, props);
 
     this.state = this.settings.state;
   }
 
   ref = () => {
-    return React.createRef();
-  };
+    return React.createRef()
+  }
 
   clickSelfIcon = () => {
-    return <Navigate to="/settings" />;
-  };
+    return (<Navigate to='/settings' />);
+  }
 
-  componentDidMount() {
+  componentDidMount () {
     // this.startProgress();
 
     // $('.ui.sidebar').sidebar();
@@ -96,112 +93,81 @@ class Dashboard extends React.Component {
   handleLogout = () => {
     this.setState({
       loading: true,
-      isLoggingOut: true,
+      isLoggingOut: true
     });
 
     setTimeout(() => {
       this.props.onLogoutSuccess();
       this.setState({
         loading: false,
-        isLoggingOut: false,
+        isLoggingOut: false
       });
     }, 500);
   };
 
-  handleSettings = () => {};
+  handleSettings = () => {
+
+  }
 
   handleSidebarToggle = () => {
     this.setState((prevState) => ({
-      sidebarCollapsed: !prevState.sidebarCollapsed,
+      sidebarCollapsed: !prevState.sidebarCollapsed
     }));
   };
 
   startProgress = () => {
     this.intervalId = setInterval(() => {
-      this.setState(
-        (prevState) => ({
-          progress: prevState.progress + 1,
-        }),
-        () => {
-          if (this.state.progress >= 100) {
-            this.completeProgress();
-            this.setState({ isLoading: false });
-            clearInterval(this.intervalId);
-          } else {
-            this.ref.current.continuousStart();
-          }
+      this.setState(prevState => ({
+        progress: prevState.progress + 1,
+      }), () => {
+        if (this.state.progress >= 100) {
+          this.completeProgress();
+          this.setState({ isLoading: false });
+          clearInterval(this.intervalId);
+        } else {
+          this.ref.current.continuousStart();
         }
-      );
+      });
     }, 5);
   };
 
   completeProgress = () => {
     this.ref.current.complete();
   };
-
+  
   handleSearchChange = (e) => {
-    console.log("search change:", e);
+    console.log('search change:', e);
     this.setState({ search: e.target.value });
   };
 
   toggleSidebar = (e) => {
-    $(".ui.sidebar").sidebar("toggle");
-  };
+    $('.ui.sidebar').sidebar('toggle');
+  }
 
-  render() {
-    const sidebarStyle = this.state.sidebarCollapsed ? { width: "auto" } : {};
+  render () {
+    const sidebarStyle = this.state.sidebarCollapsed ? { width: 'auto' } : {};
 
     return (
-      <jeeves-dashboard style={{ height: "100%" }} className="fade-in">
+      <jeeves-dashboard style={{ height: '100%' }} className='fade-in'>
         {/* <LoadingBar color="#f11946" progress={this.state.progress} /> */}
         {/* <Joyride steps={this.state.steps} /> */}
-        <div
-          id="sidebar"
-          attached="bottom"
-          style={{
-            overflow: "hidden",
-            borderRadius: 0,
-            height: "100vh",
-            backgroundColor: "#eee",
-          }}
-        >
-          <Sidebar
-            as={Menu}
-            icon="labeled"
-            inverted
-            vertical
-            visible={true}
-            style={sidebarStyle}
-            width="wide"
-            size="huge"
-          >
+        <div id="sidebar" attached="bottom" style={{ overflow: 'hidden', borderRadius: 0, height: '100vh', backgroundColor: '#eee' }}>
+          <Sidebar as={Menu} icon='labeled' inverted vertical visible={true} style={sidebarStyle} width='wide' size='huge'>
             <Menu.Item as={Link} to="/">
               <Header inverted>
-                <Popup
-                  trigger={
-                    <Icon name="help" size="small" style={{ float: "right" }} />
-                  }
-                >
+                <Popup trigger={
+                  <Icon name='help' size='small' style={{ float: 'right' }} />
+                }>
                   <Popup.Header>Need Help?</Popup.Header>
                   <Popup.Content>
-                    <p>
-                      Send us an email:{" "}
-                      <a href="mailto:support@jeeves.dev">support@jeeves.dev</a>
-                    </p>
+                    <p>Send us an email: <a href="mailto:support@jeeves.dev">support@jeeves.dev</a></p>
                     {/* <p><strong>Call Chuck!</strong> +1 (d00) p00-d00p</p> */}
                   </Popup.Content>
                 </Popup>
-                <img
-                  src="/images/jeeves-tux.png"
-                  class="icon"
-                  style={{
-                    height: "1.2em",
-                    width: "1.2em",
-                    verticalAlign: "top",
-                  }}
-                />{" "}
-                J{this.state.sidebarCollapsed ? "" : "EEVES"}
-                <Popup trigger={<Label>alpha</Label>}>
+                <img src="/images/jeeves-tux.png" class="icon" style={{ height: '1.2em', width: '1.2em', verticalAlign: 'top' }} /> J{this.state.sidebarCollapsed ? '' : 'EEVES'}
+                <Popup trigger={
+                  <Label>alpha</Label>
+                }>
                   <Popup.Content>Exclusive access!</Popup.Content>
                 </Popup>
               </Header>
@@ -215,35 +181,19 @@ class Dashboard extends React.Component {
               </jeeves-search>
             </Menu.Item> */}
             <Menu.Item as={Link} to="/">
-              <div>
-                <Icon name="home" /> {!this.state.sidebarCollapsed && "Home"}
-              </div>
+              <div><Icon name='home' /> {!this.state.sidebarCollapsed && 'Home'}</div>
             </Menu.Item>
-            <Menu.Item as={Link} to="/cases">
-              <div>
-                <Icon name="briefcase" />{" "}
-                {!this.state.sidebarCollapsed && "Cases"}
-              </div>
+            <Menu.Item as={Link} to='/cases'>
+              <div><Icon name='briefcase' /> {!this.state.sidebarCollapsed && 'Cases'}</div>
             </Menu.Item>
             <Menu.Item as={Link} to="/conversations">
-              <div>
-                <Icon name="quote left" />{" "}
-                {!this.state.sidebarCollapsed && "Conversations"}{" "}
-                {this.state.conversationAlert ? (
-                  <Label size="mini" color="red">
-                    !
-                  </Label>
-                ) : null}
-              </div>
+              <div><Icon name='quote left' /> {!this.state.sidebarCollapsed && 'Conversations'} {this.state.conversationAlert ? <Label size='mini' color='red'>!</Label>: null}</div>
             </Menu.Item>
             <Menu.Item disabled>
               <div>
-                <Icon name="book" />
-                {!this.state.sidebarCollapsed && "Library"}
-                &nbsp;
-                <Label size="mini" color="orange">
-                  disabled
-                </Label>
+                <Icon name='book' />
+                {!this.state.sidebarCollapsed && 'Library'}
+                &nbsp;<Label size='mini' color='orange'>disabled</Label>
               </div>
             </Menu.Item>
             {/* <Menu.Item disabled>
@@ -256,81 +206,38 @@ class Dashboard extends React.Component {
               <div><Icon name={this.state.sidebarCollapsed ? 'arrow right' : 'arrow left'} /> {this.state.sidebarCollapsed ? '' : 'Collapse'}</div>
             </Menu.Item> */}
             <Menu.Item as={Link} to="/settings">
-              <div>
-                <Icon name="cog" /> {!this.state.sidebarCollapsed && "Settings"}
-              </div>
+              <div><Icon name='cog' /> {!this.state.sidebarCollapsed && 'Settings'}</div>
             </Menu.Item>
-            {this.props.auth.isAdmin ? (
-              <Menu.Item as={Link} to="/settings/admin">
-                <div>
-                  <Icon name="hammer" />{" "}
-                  {!this.state.sidebarCollapsed && "Admin"}
-                </div>
-              </Menu.Item>
-            ) : null}
-            <Menu.Item
-              as={Link}
-              to="/"
-              onClick={this.handleLogout}
-              loading={this.state.isLoggingOut}
-            >
-              <div>
-                <Icon name="sign-out" />{" "}
-                {!this.state.sidebarCollapsed && "Logout"}
-              </div>
+            {(this.props.auth.isAdmin) ? (<Menu.Item as={Link} to="/settings/admin">
+              <div><Icon name='hammer' /> {!this.state.sidebarCollapsed && 'Admin'}</div>
+            </Menu.Item>) : null}
+            <Menu.Item as={Link} to="/" onClick={this.handleLogout} loading={this.state.isLoggingOut}>
+              <div><Icon name="sign-out" /> {!this.state.sidebarCollapsed && 'Logout'}</div>
             </Menu.Item>
             <Menu.Item style={{ borderBottom: 0 }}>
               <Bridge />
               {/* <p><small><Link to='/contracts/terms-of-use'>Terms of Use</Link></small></p> */}
-              <p style={{ marginTop: "2em" }}>
-                <small class="subtle">
-                  &copy; 2023 Legal Tools &amp; Technology, Inc.
-                </small>
-              </p>
-              <p>
-                <Label>
-                  <strong>Status:</strong> {this.props.status || "disconnected"}
-                </Label>
-              </p>
+              <p style={{ marginTop: '2em' }}><small class="subtle">&copy; 2023 Legal Tools &amp; Technology, Inc.</small></p>
+              <p><Label><strong>Status:</strong> {this.props.status || 'disconnected'}</Label></p>
             </Menu.Item>
           </Sidebar>
         </div>
-        <div
-          id="main-content"
-          style={{ marginLeft: "350px", paddingRight: "1em" }}
-        >
-          <Container fluid style={{ margin: "1em" }}>
-            {/* <Button className='mobile-only'><Icon name='ellipsis horizontal' /></Button> */}
-            {this.state.debug ? (
-              <div>
-                <strong>
-                  <code>isAdmin</code>:
-                </strong>{" "}
-                <span>{this.props.isAdmin ? "yes" : "no"}</span>
-                <br />
-                <strong>
-                  <code>isCompliant</code>:
-                </strong>{" "}
-                <span>{this.props.isCompliant ? "yes" : "no"}</span>
-                <br />
-                <strong>
-                  <code>auth</code>:
-                </strong>{" "}
-                <code>
-                  {this.props.auth
-                    ? JSON.stringify(this.props.auth, null, "  ")
-                    : "undefined"}
-                </code>
-                {/* <strong><code>state.auth.isAdmin</code></strong> <span>{this.state.auth.isAdmin}</span>
+        <div id="main-content" style={{ marginLeft: '350px', paddingRight: '1em' }}>
+            <Container fluid style={{ margin: '1em' }}>
+              {/* <Button className='mobile-only'><Icon name='ellipsis horizontal' /></Button> */}
+              {this.state.debug ? (
+                <div>
+                  <strong><code>isAdmin</code>:</strong> <span>{(this.props.isAdmin) ? 'yes' : 'no'}</span><br />
+                  <strong><code>isCompliant</code>:</strong> <span>{(this.props.isCompliant) ? 'yes' : 'no'}</span><br />
+                  <strong><code>auth</code>:</strong> <code>{(this.props.auth) ? JSON.stringify(this.props.auth, null, '  ') : 'undefined'}</code>
+                  {/* <strong><code>state.auth.isAdmin</code></strong> <span>{this.state.auth.isAdmin}</span>
                   <strong><code>state.auth.isCompliant</code></strong> <span>{this.state.auth.isCompliant}</span> */}
-              </div>
-            ) : null}
-            {this.state.isLoading ? null : (
-              <Routes>
-                <Route path="*" element={<Navigate to="/" replace />} />
-                <Route
-                  path="/"
-                  element={
+                </div>
+              ) : null}
+              {this.state.isLoading ? null : (
+                <Routes>
+                  <Route path="*" element={<Navigate to='/' replace />} />
+                  <Route path="/" element={
                     <Home
                       auth={this.props.auth}
                       fetchConversations={this.props.fetchConversations}
@@ -340,86 +247,19 @@ class Dashboard extends React.Component {
                       resetChat={this.props.resetChat}
                       chat={this.props.chat}
                     />
-                  }
-                />
-                <Route path="/workspaces" element={<Workspaces />} />
-                <Route
-                  path="/cases/:id"
-                  element={
-                    <CaseView
-                      fetchCase={this.props.fetchCase}
-                      cases={this.props.cases}
-                      getMessages={this.props.getMessages}
-                      submitMessage={this.props.submitMessage}
-                      fetchConversations={this.props.fetchConversations}
-                      onMessageSuccess={this.props.onMessageSuccess}
-                      resetChat={this.props.resetChat}
-                      chat={this.props.chat}
-                    />
-                  }
-                />
-                <Route
-                  path="/cases"
-                  element={
-                    <CaseHome
-                      cases={this.props.cases}
-                      fetchCases={this.props.fetchCases}
-                    />
-                  }
-                />
-                <Route
-                  path="/conversations/:id"
-                  element={
-                    <Room
-                      conversation={this.props.conversation}
-                      fetchConversation={this.props.fetchConversation}
-                      chat={this.props.chat}
-                      getMessages={this.props.getMessages}
-                      submitMessage={this.props.submitMessage}
-                    />
-                  }
-                />
-                <Route
-                  path="/conversations"
-                  element={
-                    <Conversations
-                      conversations={this.props.conversations}
-                      fetchConversations={this.props.fetchConversations}
-                    />
-                  }
-                />
-                <Route
-                  path="/settings"
-                  element={
-                    <Settings
-                      {...this.props}
-                      auth={this.props.auth}
-                      login={this.props.login}
-                    />
-                  }
-                />
-                <Route
-                  path="/settings/admin"
-                  element={
-                    <AdminSettings
-                      {...this.props}
-                      fetchAdminStats={this.props.fetchAdminStats}
-                    />
-                  }
-                />
-                <Route
-                  path="/contracts/terms-of-use"
-                  element={
-                    <TermsOfUse
-                      {...this.props}
-                      fetchContract={this.props.fetchContract}
-                    />
-                  }
-                />
-              </Routes>
-            )}
-          </Container>
-        </div>
+                  } />
+                  <Route path="/workspaces" element={<Workspaces />} />
+                  <Route path="/cases/:id" element={<CaseView fetchCase={this.props.fetchCase} cases={this.props.cases} getMessages={this.props.getMessages} submitMessage={this.props.submitMessage} fetchConversations={this.props.fetchConversations} onMessageSuccess={this.props.onMessageSuccess} resetChat={this.props.resetChat} chat={this.props.chat}/>}/>
+                  <Route path="/cases" element={<CaseHome cases={this.props.cases} fetchCases={this.props.fetchCases} />} />
+                  <Route path="/conversations/:id" element={<Room conversation={this.props.conversation} fetchConversation={this.props.fetchConversation} chat={this.props.chat} getMessages={this.props.getMessages} submitMessage={this.props.submitMessage} />} />
+                  <Route path="/conversations" element={<Conversations conversations={this.props.conversations} fetchConversations={this.props.fetchConversations} />} />
+                  <Route path="/settings" element={<Settings {...this.props} auth={this.props.auth} login={this.props.login} />} />
+                  <Route path="/settings/admin" element={<AdminSettings {...this.props} fetchAdminStats={this.props.fetchAdminStats} />} />
+                  <Route path="/contracts/terms-of-use" element={<TermsOfUse {...this.props} fetchContract={this.props.fetchContract} />} />
+                </Routes>
+              )}
+            </Container>
+          </div>
       </jeeves-dashboard>
     );
   }
