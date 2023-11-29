9032bd5e98a908b157fd7e0e7256e2c3d60d5f16
nahuelcodingit <nahuel@codingit.dev>
2023-11-29 13:47:49 -0300

    added date to conversatio, pagination, divider

--- components/Conversations.js
+++ components/Conversations.js
@@ -3,18 +3,37 @@
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const { Link } = require('react-router-dom');
-
-const {
-  Segment
-} = require('semantic-ui-react');
+const { Segment, Pagination, Divider } = require('semantic-ui-react');
 
 class Conversations extends React.Component {
-  componentDidMount () {
-    this.props.fetchConversations();
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      itemsPerPage: 15,
+      windowWidth: window.innerWidth
+    };
   }
 
-  render () {
+  componentDidMount() {
+    this.props.fetchConversations();
+    window.addEventListener('resize', this.handleResize);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handlePaginationChange = (e, { activePage }) => {
+    this.setState({ currentPage: activePage });
+  };
+  handleResize = () => {
+    this.setState({ windowWidth: window.innerWidth });
+  };
+
+  render() {
     const { loading, error, conversations } = this.props;
+    const { currentPage, itemsPerPage, windowWidth } = this.state;
+
 
     if (loading) {
       return <div>Loading...</div>;
@@ -24,21 +43,36 @@ class Conversations extends React.Component {
       return <div>Error: {error}</div>;
     }
 
+    // Calculate conversations for current page
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentConversations = conversations.slice(indexOfFirstItem, indexOfLastItem);
+
     return (
       <Segment className='fade-in' fluid style={{ marginRight: '1em' }}>
         <h2>Conversations</h2>
-        {conversations && conversations.length > 0 && conversations.map(conversation => (
+        {currentConversations.map(conversation => (
           <div key={conversation.id}>
-            <h3><Link to={'/conversations/' + conversation.id}>{conversation.title}</Link></h3>
-            <p>{conversation.content}</p>
+            <h4>
+              <Link to={'/conversations/' + conversation.id}>
+                {new Date(conversation.created_at).toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' })}{": "}
+                {conversation.title}
+              </Link>
+            </h4>
+            {/* <p>{conversation.content}</p> */}
+           <Divider />
           </div>
         ))}
+        <Pagination
+          size='tiny'          
+          activePage={currentPage}
+          totalPages={Math.ceil(conversations.length / itemsPerPage)}
+          onPageChange={this.handlePaginationChange}
+          ellipsisItem={(windowWidth>480)? undefined : null}
+          boundaryRange={(windowWidth>480) ? 1 : 0}
+        />
       </Segment>
     );
-  }
-
-  toHTML () {
-    return ReactDOMServer.renderToString(this.render());
   }
 }
 
