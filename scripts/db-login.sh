242d760401d5c10c454c697c70947034450d24b1
Neil Player <n.player89@gmail.com>
2024-02-15 17:44:31 +0000

    chage to get password from env

--- scripts/db-login.sh
+++ scripts/db-login.sh
@@ -2,4 +2,4 @@
 
 
 DB_IP=$(kubectl get svc -n mysql | cut -d' ' -f7)
-mysql -h $DB_IP -u db_user_jeeves -pweedis6uozedaiwee4Eereesheequiem db_jeeves
+mysql -h $DB_IP -u db_user_jeeves -p$DB_PASS db_jeeves
