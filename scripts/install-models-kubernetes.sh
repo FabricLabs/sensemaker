9daba66684fd594842d8fa6f2b66b82dcb6c90cb
nplayer89 <49922233+nplayer89@users.noreply.github.com>
2024-03-04 10:04:44 -0700

    Update install-models-kubernetes.sh

--- scripts/install-models-kubernetes.sh
+++ scripts/install-models-kubernetes.sh
@@ -4,5 +4,5 @@
 POD_NAME=$(kubectl get pods -n ollama | tail -n +2 | cut -d ' ' -f 1)
 
 echo "[+] Copying install script to $POD_NAME"
-kubectl cp install-models.sh $POD_NAME:/install-models.sh -n ollama
+kubectl cp scripts/install-models.sh $POD_NAME:/install-models.sh -n ollama
 kubectl exec -n ollama $POD_NAME -- /bin/bash -c '/install-models.sh'
