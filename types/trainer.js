b851d7192b01367dbd60800ef1199ea9f84bed8c
Eric Martindale <eric@ericmartindale.com>
2024-03-17 14:50:06 -0500

    Fix Redis issue

--- types/trainer.js
+++ types/trainer.js
@@ -84,6 +84,7 @@ class Trainer extends Service {
 
     this.loader = new DirectoryLoader(this.settings.store.path, this.loaders);
 
+    // Redis Client
     this.redis = createClient({
       username: this.settings.redis.username,
       password: this.settings.redis.password,
@@ -131,7 +132,7 @@ class Trainer extends Service {
      * In Node Redis, if you handle add the on('error') stuff it will queue up your commands and then in the background try to reconnect. When it does reconnect, it will then run the queued commands. [7:30 PM] It actually follows a retry_strategy to attempt to reconnect. You can read all about it on the old README at https://www.npmjs.com/package/redis/v/3.1.2
      * You need to set the enable_offline_queue option to false to turn off this queueing and get an error.
      */
-    this.redis.on('error', (err) => console.error('Redis Client Error', err));
+    // this.redis.on('error', (err) => console.error('Redis Client Error', err));
 
     // Splitter for large documents
     this.splitter = new RecursiveCharacterTextSplitter({
@@ -229,13 +230,51 @@ class Trainer extends Service {
     });
   }
 
+  async ingestReferences () {
+    return new Promise((resolve, reject) => {
+      fs.readFile('./contracts/terms-of-use.md', async (error, terms) => {
+        if (error) {
+          console.error('[TRAINER]', 'Error reading terms:', error);
+          return;
+        }
+
+        console.debug('[TRAINER]', 'Terms of Use:', terms.toString('utf8'));
+
+        // Store Documents
+        // NOVO Web Application
+        // const spa = await this.ui.load();
+        // console.debug('[TRAINER]', 'SPA:', spa);
+
+        // Terms of Use
+        const contract = new Document({ pageContent: terms.toString('utf8'), metadata: { type: 'text/markdown' } });
+        const contractChunks = await this.splitter.splitDocuments([contract]);
+        // const chunks = await this.splitter.splitDocuments(spa);
+        // const allDocs = [contract].concat(spa, contractChunks, chunks);
+        // const allDocs = contractChunks.concat(chunks);
+        const allDocs = contractChunks;
+
+        // TODO: use @fabric/core/types/filesystem for a persistent log of changes (sidechains)
+        if (this.settings.debug) console.debug('[SENSEMAKER]', '[TRAINER]', '[GENESIS]', allDocs);
+
+        // Return the documents
+        resolve(allDocs);
+      });
+    });
+  }
+
   async start () {
     this._state.content.status = this._state.status = 'STARTING';
 
     // Start Services
     // Redis
     console.debug('[NOVO]', '[TRAINER]', 'Starting Redis...');
-    console.debug('[NOVO]', '[TRAINER]', 'Redis:', this.redis.options);
+    console.debug('[NOVO]', '[TRAINER]', 'Redis:', this.settings.redis);
+
+    this.redis.on('connect', async () => {
+      console.debug('[NOVO]', '[TRAINER]', 'Redis connected.');
+      await this.ingestReferences();
+      console.debug('[NOVO]', '[TRAINER]', 'Ingested references!');
+    });
 
     try {
       await this.redis.connect();
@@ -244,29 +283,19 @@ class Trainer extends Service {
       // process.exit(); // TODO: look at exit codes
     }
 
-    const terms = fs.readFileSync('./contracts/terms-of-use.md').toString('utf8');
-    const spa = await this.ui.load();
-
-    // Store Documents
-    const contract = new Document({ pageContent: terms, metadata: { type: 'text/markdown' } });
-    const contractChunks = await this.splitter.splitDocuments([contract]);
-    const chunks = await this.splitter.splitDocuments(spa);
-    const allDocs = [contract].concat(spa, contractChunks, chunks);
-
-    // TODO: use @fabric/core/types/filesystem for a persistent log of changes (sidechains)
-    if (this.settings.debug) console.debug('[SENSEMAKER]', '[TRAINER]', '[GENESIS]', allDocs);
+    const allDocs = await this.ingestReferences();
 
     this.embeddings = await RedisVectorStore.fromDocuments(allDocs, new TensorFlowEmbeddings(), {
       redisClient: this.redis,
-      indexName: 'documents'
+      indexName: this.settings.redis.indexName || 'novo-embeddings'
     });
 
-    try {
+    /* try {
       const docs = await this.loader.load();
       if (this.settings.debug) console.debug('[TRAINER]', 'Loaded documents:', docs);
     } catch (exception) {
       if (this.settings.debug) console.error('[TRAINER]', 'Error loading documents:', exception);
-    }
+    } */
 
     this.langchain = RetrievalQAChain.fromLLM(this.ollama, this.embeddings.asRetriever());
 
