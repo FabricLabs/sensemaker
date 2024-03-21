8eb7b9a4489494ac0362e2f2243d6be12e588a7d
Eric Martindale <eric@ericmartindale.com>
2024-03-21 16:54:21 -0500

    Update model list

--- scripts/install-models.sh
+++ scripts/install-models.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ollama pull llama2
+ollama pull llama2-uncensored
+ollama pull yarn-llama2:7b-128k
+ollama pull yarn-llama2:13b-64k
 ollama pull mistral
 ollama pull mixtral
 ollama pull orca2
