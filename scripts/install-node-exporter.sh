625ded8cc55940fab4c662a383ded0d9197edc39
nplayer89 <n.player89@gmail.com>
2024-04-10 13:47:51 -0600

    fix missing version variable

--- scripts/install-node-exporter.sh
+++ scripts/install-node-exporter.sh
@@ -12,7 +12,7 @@ fi
 (cd /tmp && curl -OL https://github.com/prometheus/node_exporter/releases/download/$VERSION/node_exporter-$VERSION.linux-amd64.tar.gz)
 
 # Unzip 
-(cd /tmp && tar -xvf /tmp/node_exporter-1.2.2.linux-amd64.tar.gz)
+(cd /tmp && tar -xvf /tmp/node_exporter-$VERSION.linux-amd64.tar.gz)
 
 # Create user and group for node exporter
 useradd -m node_exporter
@@ -20,7 +20,7 @@ groupadd node_exporter
 usermod -a -G node_exporter node_exporter
 
 # Move binary and set file permissions
-mv /tmp/node_exporter-${VERSION}.linux-amd64/node_exporter /usr/local/bin/
+mv /tmp/node_exporter-$VERSION.linux-amd64/node_exporter /usr/local/bin/
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 # Create systemd service file
