25d78d7a02ed98bf30f996d593b11b5161b90645
wheelspawn <nsage@protonmail.com>
2024-06-14 16:57:38 -0500

    create better version of server in Flask with two endpoints

--- scripts/server.py
+++ scripts/server.py
@@ -1,54 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+# To start the server, install the dependencies and run:
+# python3 server.py nlpaueb/legal-bert-base-uncased (or whatever model you want to use)
 
-This is a temporary script file.
-"""
+# Two endpoints are available:
 
-from transformers import BertTokenizer, BertConfig, BertModel
+# Embeddings:
+# curl -X POST -G "localhost:5000/api/embeddings" --data-urlencode "sentence=The model will return an embedding for this sentence."
 
-import json
+# Masked LM:
+# curl -X POST -G "localhost:5000/api/mask" --data-urlencode "sentence=The model will predict the [MASK]."
+
+from transformers import BertTokenizer, BertConfig, BertModel, pipeline
+
+from flask import Flask
+from flask import request
+
 from functools import cached_property
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.parse import parse_qsl, urlparse
 
-tokenizer = BertTokenizer.from_pretrained("nlpaueb/legal-bert-base-uncased")
-config_ = BertConfig.from_pretrained("nlpaueb/legal-bert-base-uncased")
-model = BertModel.from_pretrained("nlpaueb/legal-bert-base-uncased", config=config_)
+import argparse
+import json
 
-class WebRequestHandler(BaseHTTPRequestHandler):
+app = Flask(__name__)
+
+class EmbeddingModel(object):
+
+    def __init__(self,model_dir):
+        self.model_dir = model_dir
+        self.tokenizer = BertTokenizer.from_pretrained(model_dir)
+        self.config_ = BertConfig.from_pretrained(model_dir)
+        self.model = BertModel.from_pretrained(model_dir, config=self.config_)
     
-    @cached_property
-    def url(self):
-        return urlparse(self.path)
-    
-    @cached_property
-    def sentence(self):
-        return parse_qsl(self.url.query)[0]
-    
-    def get_embeddings(self):
-        encoded_input = tokenizer(self.sentence[1], return_tensors='pt')
-        
-        output = model(**encoded_input)
+    def get_embeddings(self,sentence):
+        encoded_input = self.tokenizer(sentence[1], return_tensors='pt')
+        output = self.model(**encoded_input)
         return json.dumps({ key: output[key].detach().numpy().tolist() for key in output.keys()})
-    
-    def do_POST(self):
-        self.do_GET()
-        
-    def do_GET(self):
-        
-        print(self.path)
-        print(self.url)
-        print(self.sentence)
-        
-        if self.url.path == '/api/embeddings' and self.sentence[0] == 'sentence':
-    
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(self.get_embeddings().encode("utf-8"))
+
+    def get_mask(self,sentence):
+        unmasker = pipeline('fill-mask', model=self.model_dir)
+        return json.dumps(unmasker(sentence))
+
+@app.route('/api/embeddings', methods = ['POST'])
+def api_embeddings():
+    if request.method == 'POST':
+        sentence = request.args.get('sentence', None)
+        return bert.get_embeddings(sentence).encode("utf-8")
+
+@app.route('/api/mask', methods = ['POST'])
+def api_mask():
+    if request.method == 'POST':
+        sentence = request.args.get('sentence', None)
+        return bert.get_mask(sentence).encode("utf-8")
 
 if __name__ == "__main__":
-    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
-    server.serve_forever()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_dir', help="Directory to local/Hugging Face hosted model")
+    args = parser.parse_args()
+
+    bert = EmbeddingModel(args.model_dir)
+    app.run(debug=True)
 
