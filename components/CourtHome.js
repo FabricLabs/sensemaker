8b189c8d1cbd12d56b58c0f1d7c3b38ed111cab5
nahuelcodingit <nahuel@codingit.dev>
2024-06-24 19:29:50 -0300

    court home filter update

--- components/CourtHome.js
+++ components/CourtHome.js
@@ -93,29 +93,26 @@ class CourtHome extends React.Component {
     return (
       <Segment className="fade-in" fluid style={{ maxHeight: '100%', minHeight: '100%' }}>
         <h1>Courts</h1>
-        <jeeves-search fluid placeholder='Find...' className='ui search'>
-          <div className='ui huge icon fluid input'>
-            <input
-              name='query'
-              autoComplete='off'
-              placeholder='Find...'
-              type='text'
-              tabIndex='0'
-              className='prompt'
-              //value={searchQuery}
-              onChange={(e) => {
-                const query = e.target.value;
-                this.setState({ searchQuery: query });
-                this.handleSearchChange(query); // Call the debounce function with the query
-              }}
-            />
-            <i aria-hidden="true" className="search icon"></i>
-          </div>
-        </jeeves-search>
-        <Form>
-          <Form.Dropdown
-            placeholder='Select Jurisdiction'
-            label='Jurisdiction'
+        <Form style={{ display: 'flex', gap: '1em', width: '100%' }} size='huge'>
+          <Input
+            style={{ width: '50%' }}
+            name='query'
+            autoComplete='off'
+            placeholder='Find...'
+            type='text'
+            tabIndex='0'
+            className='prompt'
+            icon='search'
+            iconPosition='right'
+            onChange={(e) => {
+              const query = e.target.value;
+              this.setState({ searchQuery: query });
+              this.handleSearchChange(query); // Call the debounce function with the query
+            }}
+          />
+          <Dropdown
+            style={{ width: '50%' }}
+            placeholder='Filter by Jurisdiction'
             fluid
             search
             selection
