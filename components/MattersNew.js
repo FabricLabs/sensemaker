233e89cf1be5ce0a681d7ed3bf5b7571ca49cdbe
nahuelcodingit <nahuel@codingit.dev>
2024-07-11 11:18:32 -0300

    removing intersitial on matter creation

--- components/MattersNew.js
+++ components/MattersNew.js
@@ -2,7 +2,7 @@
 
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const { Link, Navigate } = require('react-router-dom');
+const { Link, Navigate, useNavigate } = require('react-router-dom');
 
 const {
   Segment,
@@ -78,6 +78,7 @@ class MattersNew extends React.Component {
       if (prevProps.matters !== matters && !matters.loading) {
         if (matters.creationSuccess) {
           this.setState({ creating: false, resetFlag: true, errorCreating: '' });
+          this.props.navigateToMatter(matters.idCreated);
         } else {
           this.setState({ creating: false, errorCreating: matters.error });
         }
@@ -151,171 +152,162 @@ class MattersNew extends React.Component {
     return (
       <Segment style={{ height: '97vh', overflow: 'visible' }} className='center-elements-column'>
         <Header as='h1'>New Matter</Header>
-        {(matters && matters.creationSuccess && resetFlag && !errorCreating) ? (
-          <Message positive style={{ maxWidth: '350px' }}>
-            <Message.Header>Matter successfully created!</Message.Header>
-            <Message.Content className='center-elements-column'>
-              <p>Your Matter was created, you can add files and notes to it, you can visit your Matter page by clicking here:</p>
-              <Link to={`/matters/${matters.idCreated}`} onClick={this.resetForm}><h3>{title}</h3></Link>
-            </Message.Content>
-          </Message>
-        ) : (
-          <Form
-            onSubmit={this.handleSubmit}
-            loading={(jurisdictions.loading || courts.loading)}
-            className='ui segment new-matter-form'
-            style={{ position: 'relative', zIndex: 1000, overflow: 'visible' }}
-          >
-            <Table basic='very' stripped>
-              <Table.Body>
-                <Table.Row >
-                  <Table.Cell width={3} className='required field'>
-                    <label className='matter-label'>Matter Name</label>
-                  </Table.Cell>
-                  <Table.Cell width={12}>
-                    <Form.Input name='title' required onChange={this.handleInputChange} />
-                  </Table.Cell>
-                  <Table.Cell width={1} />
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell width={3} className='required field'>
-                    <label className='matter-label'>Plaintiff</label>
-                  </Table.Cell>
-                  <Table.Cell width={12}>
-                    <Form.Input name='plaintiff' required onChange={this.handleInputChange} />
-                  </Table.Cell>
-                  <Table.Cell width={1}>
-                    <Popup trigger={<Icon name='info circle' />}>
-                      <Popup.Content>
-                        <p>Write however these will appear in court documents</p>
-                      </Popup.Content>
-                    </Popup>
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell width={3} className='required field'>
-                    <label className='matter-label'>Defendant</label>
-                  </Table.Cell>
-                  <Table.Cell width={12}>
-                    <Form.Input name='defendant' required onChange={this.handleInputChange} />
-                  </Table.Cell>
-                  <Table.Cell width={1}>
-                    <Popup trigger={<Icon name='info circle' />}>
-                      <Popup.Content>
-                        <p>Write however these will appear in court documents</p>
-                      </Popup.Content>
-                    </Popup>
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell width={3} className='required field'>
-                    <label className='matter-label'>Who are you representing?</label>
-                  </Table.Cell>
-                  <Table.Cell width={12}>
-                    <FormField>
-                      <Checkbox
-                        radio
-                        label='Plaintiff'
-                        name='checkboxRadioGroup'
-                        value='Plaintiff'
-                        checked={representingOption === 'Plaintiff'}
-                        onChange={(e, data) => this.setState({ representingOption: data.value })}
-                      />
-                    </FormField>
-                    <FormField>
-                      <Checkbox
-                        radio
-                        label='Defendant'
-                        name='checkboxRadioGroup'
-                        value='Defendant'
-                        checked={representingOption === 'Defendant'}
-                        onChange={(e, data) => this.setState({ representingOption: data.value })}
-                      />
-                    </FormField>
-                  </Table.Cell>
-                  <Table.Cell width={1} />
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell />
-                  <Table.Cell>
-                    <Form.Dropdown
-                      placeholder='Select Jurisdiction'
-                      label='Jurisdiction'
-                      fluid
-                      search
-                      selection
-                      required
-                      options={jurisdictionsOptions}
-                      onChange={(e, { value }) => this.selectJurisdiction(value)}
-                      error={jurisdictionErrorMessage}
+        <Form
+          onSubmit={this.handleSubmit}
+          loading={(jurisdictions.loading || courts.loading)}
+          className='ui segment new-matter-form'
+          style={{ position: 'relative', zIndex: 1000, overflow: 'visible' }}
+        >
+          <Table basic='very' stripped>
+            <Table.Body>
+              <Table.Row >
+                <Table.Cell width={3} className='required field'>
+                  <label className='matter-label'>Matter Name</label>
+                </Table.Cell>
+                <Table.Cell width={12}>
+                  <Form.Input name='title' required onChange={this.handleInputChange} />
+                </Table.Cell>
+                <Table.Cell width={1} />
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell width={3} className='required field'>
+                  <label className='matter-label'>Plaintiff</label>
+                </Table.Cell>
+                <Table.Cell width={12}>
+                  <Form.Input name='plaintiff' required onChange={this.handleInputChange} />
+                </Table.Cell>
+                <Table.Cell width={1}>
+                  <Popup trigger={<Icon name='info circle' />}>
+                    <Popup.Content>
+                      <p>Write however these will appear in court documents</p>
+                    </Popup.Content>
+                  </Popup>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell width={3} className='required field'>
+                  <label className='matter-label'>Defendant</label>
+                </Table.Cell>
+                <Table.Cell width={12}>
+                  <Form.Input name='defendant' required onChange={this.handleInputChange} />
+                </Table.Cell>
+                <Table.Cell width={1}>
+                  <Popup trigger={<Icon name='info circle' />}>
+                    <Popup.Content>
+                      <p>Write however these will appear in court documents</p>
+                    </Popup.Content>
+                  </Popup>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell width={3} className='required field'>
+                  <label className='matter-label'>Who are you representing?</label>
+                </Table.Cell>
+                <Table.Cell width={12}>
+                  <FormField>
+                    <Checkbox
+                      radio
+                      label='Plaintiff'
+                      name='checkboxRadioGroup'
+                      value='Plaintiff'
+                      checked={representingOption === 'Plaintiff'}
+                      onChange={(e, data) => this.setState({ representingOption: data.value })}
                     />
-                  </Table.Cell>
-                  <Table.Cell />
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell />
-                  <Table.Cell>
-                    <Form.Dropdown
-                      placeholder='Select Court'
-                      label='Court (optional)'
-                      fluid
-                      search
-                      selection
-                      options={courtsOptions}
-                      onChange={(e, { value }) => this.setState({ court_id: value })}
-                      disabled={!this.state.jurisdiction_id || this.props.courts.loading}
+                  </FormField>
+                  <FormField>
+                    <Checkbox
+                      radio
+                      label='Defendant'
+                      name='checkboxRadioGroup'
+                      value='Defendant'
+                      checked={representingOption === 'Defendant'}
+                      onChange={(e, data) => this.setState({ representingOption: data.value })}
                     />
-                  </Table.Cell>
-                  <Table.Cell />
-                </Table.Row>
+                  </FormField>
+                </Table.Cell>
+                <Table.Cell width={1} />
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell />
+                <Table.Cell>
+                  <Form.Dropdown
+                    placeholder='Select Jurisdiction'
+                    label='Jurisdiction'
+                    fluid
+                    search
+                    selection
+                    required
+                    options={jurisdictionsOptions}
+                    onChange={(e, { value }) => this.selectJurisdiction(value)}
+                    error={jurisdictionErrorMessage}
+                  />
+                </Table.Cell>
+                <Table.Cell />
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell />
+                <Table.Cell>
+                  <Form.Dropdown
+                    placeholder='Select Court'
+                    label='Court (optional)'
+                    fluid
+                    search
+                    selection
+                    options={courtsOptions}
+                    onChange={(e, { value }) => this.setState({ court_id: value })}
+                    disabled={!this.state.jurisdiction_id || this.props.courts.loading}
+                  />
+                </Table.Cell>
+                <Table.Cell />
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell />
+                <Table.Cell>
+                  <Form.TextArea
+                    name='description'
+                    label='Description (optional)'
+                    rows={4}
+                    onChange={this.handleInputChange} />
+                </Table.Cell>
+                <Table.Cell />
+              </Table.Row>
+              {errorCreating && (
                 <Table.Row>
                   <Table.Cell />
                   <Table.Cell>
-                    <Form.TextArea
-                      name='description'
-                      label='Description (optional)'
-                      rows={4}
-                      onChange={this.handleInputChange} />
+                    <Message negative style={{ maxWidth: '500px' }}>
+                      <Message.Header content='Error Creating this Matter' />
+                      <Message.Content content={errorCreating} />
+                    </Message>
                   </Table.Cell>
                   <Table.Cell />
-                </Table.Row>
-                {errorCreating && (
-                  <Table.Row>
-                    <Table.Cell />
-                    <Table.Cell>
-                      <Message negative style={{ maxWidth: '500px' }}>
-                        <Message.Header content='Error Creating this Matter' />
-                        <Message.Content content={errorCreating} />
-                      </Message>
-                    </Table.Cell>
-                    <Table.Cell />
-                  </Table.Row>)}
-                <Table.Row>
-                  <Table.Cell />
-                  <Table.Cell >
-                    <Button.Group>
-                      <Link to={"/matters/"} disabled={this.state.creating}>
-                        <Form.Button
-                          secondary
-                          content='Cancel'
-                          disabled={this.state.creating}
-                        />
-                      </Link>
+                </Table.Row>)}
+              <Table.Row>
+                <Table.Cell />
+                <Table.Cell >
+                  <Button.Group>
+                    <Link to={"/matters/"} disabled={this.state.creating}>
                       <Form.Button
-                        primary
-                        content='Create'
-                        style={{ marginLeft: '1em' }}
-                        type='submit'
-                        loading={this.state.creating}
+                        secondary
+                        content='Cancel'
+                        disabled={this.state.creating}
                       />
-                    </Button.Group>
-                  </Table.Cell>
-                  <Table.Cell />
-                </Table.Row>
-              </Table.Body>
-            </Table>
-          </Form>
-        )}
+                    </Link>
+                    <Form.Button
+                      primary
+                      content='Create'
+                      style={{ marginLeft: '1em' }}
+                      type='submit'
+                      loading={this.state.creating}
+                    />
+                  </Button.Group>
+                </Table.Cell>
+                <Table.Cell />
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Form>
+
         {/* <Link to={"/matters/"}>Back to Matters </Link> */}
       </Segment>
     );
@@ -330,4 +322,11 @@ class MattersNew extends React.Component {
   }
 }
 
-module.exports = MattersNew;
+
+function Matter(props) {
+  const navigate = useNavigate();
+  const navigateToMatter = (id) => navigate(`/matters/${id}`);
+  return <MattersNew navigateToMatter={navigateToMatter} {...props} />;
+}
+
+module.exports = Matter;
