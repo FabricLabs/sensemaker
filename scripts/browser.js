6c4f2ffa20b76ef399e5c1f19d51ed4e926776e9
Eric Martindale <eric@ericmartindale.com>
2024-07-31 20:30:21 -0500

    Snapshot of Sensemaker changes

--- scripts/browser.js
+++ scripts/browser.js
@@ -1,9 +1,10 @@
 // Dependencies
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+// import * as React from 'react';
+// import { createRoot } from 'react-dom/client';
 
 // Components
-import Sensemaker from '../components/Sensemaker';
+// import Sensemaker from '../components/Sensemaker';
+import * as FabricChatBar from '@fabric/http/components/FabricChatBar';
 
 // Settings
 const settings = {
@@ -14,21 +15,25 @@ const settings = {
 async function main (input = {}) {
   console.log('[SENSEMAKER:BROWSER] main() executing...');
 
+  customElements.define('fabric-chat-bar', FabricChatBar);
+
   window.addEventListener('load', async () => {
     console.log('loaded!');
+
+    const container = document.getElementById('fabric-application-root');
+    // const root = createRoot(container);
+
+    const chatbar = document.createElement('fabric-chat-bar');
+
+    chatbar.style = 'position: absolute; bottom: 1em;';
+
+    container.append(chatbar);
+
+    // root.render(<Sensemaker state={input} />);
   });
 
-  const container = document.getElementById('fabric-application-root');
-  const root = createRoot(container);
-
-  const chatbar = document.createElement('fabric-chat-bar');
-  chatbar.style = 'position: absolute; bottom: 1em;';
-  document.append(chatbar);
-
-  // root.render(<Sensemaker state={input} />);
-
   return {
-    react: { root }
+    // react: { root }
   }
 }
 
