90e57d1bc7fd7ccb8aefe75132c1357b44fa9f34
Eric Martindale <eric@ericmartindale.com>
2024-12-06 05:04:11 -0800

    Re-work blob storage

--- routes/services/rsi/create_activity.js
+++ routes/services/rsi/create_activity.js
@@ -5,15 +5,12 @@ const Actor = require('@fabric/core/types/actor');
 
 module.exports = async function (req, res, next) {
   const proposal = merge({}, req.body);
-  const actor = new Actor(proposal);
-  console.debug('[SENSEMAKER]', 'Creating activity:', proposal);
-
+  const content = JSON.stringify(proposal, null, '  ');
+  const actor = new Actor({ content: content });
   // TODO: add sha256
   const blobs = await this.db('blobs').insert({
     fabric_id: actor.id,
     content: JSON.stringify(proposal)
   });
-  console.debug('blobs:', blobs);
-
-  res.send({ message: 'Activity created!' });
+  return res.send({ message: 'Activity created!' });
 };
